7cb31bfbafcec8ec83502ce1bc01d702227e76fc
John Ore <bobwear@gmail.com>
2026-09-15 13:49:02 -0400

    Remove Vue component

--- cmd-k-engine/src/components/CommandFooter.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="palette-footer">
-    <span>↑ ↓ Navigate</span>
-    <span>Enter Select</span>
-    <span>Esc Close</span>
-    <span>⌫ Back</span>
-  </footer>
-</template>
